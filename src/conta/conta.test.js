8c020945574137071800fb1fda380e6e59329e5f
Sandro <sandro.luiz@hvex.com.br>
2021-11-10 09:45:42 -0300

    [CREATE] react-testes-curso-alura

---
+++ src/conta/conta.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Conta from "./Conta"
+
+describe('Compoente de conta',  () => {
+    it('Exibir o saldo da conta como valor monetário',  () => {
+        render(<Conta saldo={1000}/>);
+
+        const saldo = screen.getByTestId('saldo-conta')
+        
+
+        expect(saldo.textContent).toBe('R$ 1000')
+    })
+
+    it('Chama a função de realizar transação, quando o botão é clicado', 
+    () =>{
+
+        const funcaoRealizarTransacao = jest.fn()
+
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransacao} />);
+
+        fireEvent.click(screen.getByText('Realizar operação'))
+
+        expect(funcaoRealizarTransacao).toHaveBeenCalled()
+    })
+})
